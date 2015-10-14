a7cf75a383dec8807f6cdf5112aba2c2562cba95
eve <ganthima_st@hotmail.com>
2015-10-13 17:11:54 -0700

    start day2

--- app/src/main/java/com/example/eve/myapplication/MainActivity.java
+++ app/src/main/java/com/example/eve/myapplication/MainActivity.java
@@ -55,25 +55,5 @@ public class MainActivity extends AppCompatActivity {
         objtimeTABLE = new timeTABEL(this);
     }//connectedDatabase
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    
 }//Main Class
