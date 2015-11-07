fc5f0205b7c687f4c98a2ab43357c9d4b93fa7fc
eve <ganthima_st@hotmail.com>
2015-11-06 23:56:08 -0800

    Create Cursor finish

--- app/src/main/java/com/example/eve/myapplication/DetaildayActivity.java
+++ app/src/main/java/com/example/eve/myapplication/DetaildayActivity.java
@@ -1,17 +1,22 @@
 package com.example.eve.myapplication;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DetaildayActivity extends AppCompatActivity {
 
     //Explicit
     private TextView dateTextView;
     private ListView dateListView;
+    private String strDate,TAG="eve";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +27,35 @@ public class DetaildayActivity extends AppCompatActivity {
         bindWidter();
 
         showDate();
+
+        //Create List Dailyplan
+
+        createListDailyPlan();
     }//onCreate
+
+    private void createListDailyPlan() {
+
+        String[]strName =null;
+        String[] strCount = null;
+
+
+        try {
+            SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase("my_time.db",MODE_PRIVATE,null);
+            Cursor objcursor = objSqLiteDatabase.rawQuery("SELECT * FROM timeTABLE WHERE date = " + "'" + strDate + "'",null);
+            objcursor.moveToFirst();
+            strName=new String[objcursor.getCount()];
+            strCount=new String[objcursor.getCount()];
+            Log.d(TAG,"จำนวน Cursor ==> "+ Integer.toString(objcursor.getCount()));
+
+        }catch (Exception e){
+            Toast.makeText(DetaildayActivity.this,"วันนี้ไม่มีงานอะไร",Toast.LENGTH_LONG).show();
+        }
+
+    }//Create List Dailyplan
 
     private void showDate() {
 
-        String strDate = getIntent().getStringExtra("Date");
+        strDate = getIntent().getStringExtra("Date");
         dateTextView.setText(strDate);
     }
 
